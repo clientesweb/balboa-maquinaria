115727378b0c4982bb9dc6e656ecc63685abe9d2
clientesweb <alexisifran32@gmail.com>
2025-08-23 18:59:50 -0300

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <main className="min-h-screen">
       <Header />
-      <VideoBanner />
+      <VideoBanner
       <AboutSection />
       <BrandsCarousel />
       <FeaturedProducts />
